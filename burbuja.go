5a32cda6fb4d7b44c0c5f0e49a8746feb00b2153
efren <efren.hernandez@alumnos.udg.mx>
2020-10-19 14:13:43 -0500

    V2

--- burbuja.go
+++ burbuja.go
@@ -2,12 +2,12 @@ package main
 
 func Burbuja(s []int64) {
 	var dummy int64
-	for i := 0; i < len(s); i++ {
-		for j := 0; j < len(s); j++ {
-			if s[i] > s[j] {
-				dummy = s[i]
-				s[i] = s[j]
-				s[j] = dummy
+	for i := 1; i < len(s); i++ {
+		for j := 0; j < (len(s) - i); j++ {
+			if s[j] > s[j+1] {
+				dummy = s[j]
+				s[j] = s[j+1]
+				s[j+1] = dummy
 			}
 		}
 	}
